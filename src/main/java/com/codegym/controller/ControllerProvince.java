c4b8a425f560175002233f49a98cda0e1e798a67
DuyTran1811 <ferssken99@gmail.com>
2021-04-07 17:13:36 +0700

    show create province

--- src/main/java/com/codegym/controller/ControllerProvince.java
+++ src/main/java/com/codegym/controller/ControllerProvince.java
@@ -5,6 +5,8 @@ import com.codegym.service.iml.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,6 +19,22 @@ public class ControllerProvince {
         Iterable<Province> provinces = provinceService.findAll();
         ModelAndView modelAndView = new ModelAndView("province/list");
         modelAndView.addObject("province", provinces);
+        return modelAndView;
+    }
+
+    @GetMapping("create-province")
+    ModelAndView showCreateProvince() {
+        ModelAndView modelAndView = new ModelAndView("province/create");
+        modelAndView.addObject("province", new Province());
+        return modelAndView;
+    }
+
+    @PostMapping("create-province")
+    public ModelAndView createProvince(@ModelAttribute("province") Province province) {
+        provinceService.save(province);
+        ModelAndView modelAndView = new ModelAndView("province/create");
+        modelAndView.addObject("province", new Province());
+        modelAndView.addObject("message", "New Province created successfully");
         return modelAndView;
     }
 }
