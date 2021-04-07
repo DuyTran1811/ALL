a7b1cfed2a6b06b358e3d74d62e6e44c74369256
DuyTran1811 <ferssken99@gmail.com>
2021-04-07 14:44:01 +0700

    create create customer

--- src/main/java/com/codegym/controller/ControllerCustomer.java
+++ src/main/java/com/codegym/controller/ControllerCustomer.java
@@ -7,6 +7,7 @@ import com.codegym.service.iml.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,11 +16,35 @@ public class ControllerCustomer {
     @Autowired
     private CustomerService customerService;
 
+    @Autowired
+    private ProvinceService provinceService;
+
+    @ModelAttribute("provinces")
+    public Iterable<Province> provinces() {
+        return provinceService.findAll();
+    }
+
     @GetMapping("customers")
     public ModelAndView showCustomer() {
         Iterable<Customer> customers = customerService.findAll();
         ModelAndView modelAndView = new ModelAndView("customer/list");
         modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("create")
+    public ModelAndView showCustomerCreate() {
+        ModelAndView modelAndView = new ModelAndView("customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("create-customer")
+    public ModelAndView createCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer created successfully");
         return modelAndView;
     }
 }
